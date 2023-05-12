41637866a78b7aa47c9a467d379ccb3c3f35e83d
marcosraach.1@gmail.com <marcosraach.1@gmail.com>
2023-05-12 18:21:21 -0300

    beecrowd1143 novo

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -10,13 +10,13 @@ import java.io.PrintStream;
 public class GreeterTest {
 
     private Greeter greeter = new Greeter();
-    private Beecrowd_1143 teste1 = new Beecrowd_1143();
-    private Beecrowd_1143 teste2 = new Beecrowd_1143();
-    private Beecrowd_1143 teste3 = new Beecrowd_1143();
-    private Beecrowd_1143 teste4 = new Beecrowd_1143();
+    private Beecrowd_1143 teste1Beecrowd1143 = new Beecrowd_1143();
+    private Beecrowd_1143 teste2Beecrowd1143 = new Beecrowd_1143();
+    private Beecrowd_1143 teste3Beecrowd1143 = new Beecrowd_1143();
+    private Beecrowd_1143 teste4Beecrowd1143 = new Beecrowd_1143();
 
     @Test
-    public void teste1() {
+    public void teste1Beecrowd1143() {
         String input = "5\n";
         String expectedOutput = "1 1 1\n2 4 8\n3 9 27\n4 16 64\n5 25 125\n";
 
@@ -32,7 +32,7 @@ public class GreeterTest {
     }
 
     @Test
-    public void teste2() {
+    public void teste2Beecrowd1143() {
         String input = "6\n";
         String expectedOutput = "1 1 1\n2 4 8\n3 9 27\n4 16 64\n5 25 125\n6 36 216\n";
 
@@ -48,7 +48,7 @@ public class GreeterTest {
     }
 
     @Test
-    public void teste3() {
+    public void teste3Beecrowd1143() {
         String input = "8\n";
         String expectedOutput = "1 1 1\n2 4 8\n3 9 27\n4 16 64\n5 25 125\n6 36 216\n7 49 343\n8 64 512\n";
 
@@ -64,7 +64,7 @@ public class GreeterTest {
     }
 
     @Test
-    public void teste4() {
+    public void teste4Beecrowd1143() {
         String input = "15\n";
         String expectedOutput = "1 1 1\n2 4 8\n3 9 27\n4 16 64\n5 25 125\n6 36 216\n7 49 343\n8 64 512\n9 81 729\n10 100 1000\n11 121 1331\n12 144 1728\n13 169 2197\n14 196 2744\n15 225 3375\n";
 
@@ -80,7 +80,7 @@ public class GreeterTest {
     }
 
     @Test
-    public void teste5() {
+    public void teste5Beecrowd1143() {
         String input = "20\n";
         String expectedOutput = "1 1 1\n2 4 8\n3 9 27\n4 16 64\n5 25 125\n6 36 216\n7 49 343\n8 64 512\n9 81 729\n10 100 1000\n11 121 1331\n12 144 1728\n13 169 2197\n14 196 2744\n15 225 3375\n16 256 4096\n17 289 4913\n18 324 5832\n19 361 6859\n20 400 8000\n";
 
