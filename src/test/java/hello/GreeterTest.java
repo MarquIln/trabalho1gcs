964381bec75d19c50225d57797d82c7050da761c
Artur S <110683450+Artituis@users.noreply.github.com>
2023-05-12 18:21:34 -0300

    Update GreeterTest.java

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -10,15 +10,15 @@ public class GreeterTest {
     private Beecrowd_1133 greeter = new Beecrowd_1133();
 
     @Test
-    public void teste1() {
+    public void teste_Beecrowd_1133_1() {
         assertEquals(12, Beecrowd_1133.solucao(10,18));
     }
     @Test
-    public void teste2() {
+    public void teste_Beecrowd_1133_2() {
         assertEquals(22, Beecrowd_1133.solucao(20,30));
     }
     @Test
-    public void teste3() {
+    public void teste_Beecrowd_1133_3() {
         assertEquals(22, Beecrowd_1133.solucao(30,20));
     }
 }
